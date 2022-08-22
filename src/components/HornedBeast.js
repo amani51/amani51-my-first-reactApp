369136df44ede8cef18e35ff1813a8afa1620bc3
amani51 <amani.898919@gmail.com>
2022-08-22 17:17:35 +0300

    class02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,24 +1,37 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+// import CardGroup from 'react-bootstrap/CardGroup';
 
 class HornedBeast extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>
-           HornedBeast: {this.props.title}
-        </h2>
-        <img
-          src={this.props.imageUrl}
-          alt={this.props.title}
-          title={this.props.title}
-        ></img>
-        <p>
-            {this.props.description }
-        </p>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: "",
+    };
   }
 
+  favoriteImage = () => {
+    this.setState({
+      favorite: this.state.favorite + "⭐️",
+    });
+  };
+
+  render() {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src={this.props.imageUrl}
+          onClick={this.favoriteImage}
+        />
+        <Card.Body>
+          <Card.Title>{this.props.Title}</Card.Title>
+          <Card.Text>{this.props.description }<br/>  Favorites:  {this.state.favorite}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default HornedBeast;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,68 @@
-import React from "react"
+import React from "react";
 import HornedBeast from "./HornedBeast";
-import image from "./assets/Unicorn.jpg"
-import image2 from "./assets/wholesale-halloween-costume-prop-unicorn.jpg"
-import image3 from "./assets/photo-1512636618879-bbe79107e9e3.avif"
-class Main extends React.Component{
-    render(){
-        return(
-            <div>
-                <HornedBeast title=" UniWhale"  imageUrl={image}  description="A unicorn and uniWhale nuzzling their horns"/>
-                <HornedBeast title=" Rhino Family"  imageUrl={image3}  description="Parent rhino with two babies"/>
-                <HornedBeast title=" Unicorn Head"  imageUrl={image2}  description="Someone wearing a very silly unicorn head mask"/>
-            </div>
-        );
-    }
+import Cards from "./data.json";
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        {Cards.map((cards) => {
+          if (cards.horns == 1) {
+            return (
+              <div
+                style={{ display: "flex", alignItem: "center", float: "left" }}
+              >
+                <HornedBeast
+                  title={cards.title}
+                  imageUrl={cards.image_url}
+                  description={cards.description}
+                />
+                
+              </div>
+            );
+          }
+          if (cards.horns == 2) {
+            return (
+              <div
+                style={{ display: "flex", alignItem: "center", float: "left" }}
+              >
+                <HornedBeast
+                  title={cards.title}
+                  imageUrl={cards.image_url}
+                  description={cards.description}
+                />
+              </div>
+            );
+          }
+          if (cards.horns == 3) {
+            return (
+              <div
+                style={{ display: "flex", alignItem: "center", float: "left" }}
+              >
+                <HornedBeast
+                  title={cards.title}
+                  imageUrl={cards.image_url}
+                  description={cards.description}
+                />
+              </div>
+            );
+          }
+          if (cards.horns == 100) {
+            return (
+              <div
+                style={{ display: "inline-block", alignItem: "center" }}
+              >
+                
+                <HornedBeast
+                  title={cards.title}
+                  imageUrl={cards.image_url}
+                  description={cards.description}
+                />
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
 }
-
 export default Main;
