14d790d8d3287a9780b4763ff46d86b4358bbfc9
amani51 <amani.898919@gmail.com>
2022-08-22 10:25:01 +0300

    class01v2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from "react"
 import HornedBeast from "./HornedBeast";
 import image from "./Unicorn.jpg"
+import image2 from "./wholesale-halloween-costume-prop-unicorn.jpg"
 class Main extends React.Component{
     render(){
         return(
             <div>
                 <HornedBeast title=" UniWhale" imageUrl={image}  description="A unicorn and uniWhale nuzzling their horns"/>
+                <HornedBeast title=" Unicorn Head" imageUrl={image2}  description="Someone wearing a very silly unicorn head mask"/>
             </div>
         );
     }
